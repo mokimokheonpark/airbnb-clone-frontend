a3c5bb94cc0c21d14b5f39761c33ea477ed55b2c
Mokheon Park <mokimokheonpark@gmail.com>
2023-08-16 14:52:29 -0400

    set up

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div>
+      <Text color={"blue.500"} fontSize={"5xl"}>
+        Hello, World!
+      </Text>
+    </div>
+  );
+}
+
+export default App;
