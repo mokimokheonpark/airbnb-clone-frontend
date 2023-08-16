5c6a1523bb81b314a7e22651eaa0933864c1a432
Mokheon Park <mokimokheonpark@gmail.com>
2023-08-16 17:00:13 -0400

    updated NotFound

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,3 +1,16 @@
+import { Button, Heading, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <h1>404 Not Found</h1>;
+  return (
+    <VStack bg={"beige"} justifyContent={"center"} minH={"100vh"}>
+      <Heading>404. Page Not Found.</Heading>
+      <Text>The requested URL was not found on the server.</Text>
+      <Link to="/">
+        <Button colorScheme={"blue"} variant={"link"}>
+          Go Home &rarr;
+        </Button>
+      </Link>
+    </VStack>
+  );
 }
