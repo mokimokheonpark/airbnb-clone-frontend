b44ee7028975dc0c07e52b05b66d00b508956ff5
Mokheon Park <mokimokheonpark@gmail.com>
2023-08-17 12:18:31 -0400

    created header

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Box, Button, HStack } from "@chakra-ui/react";
+import { Link, Outlet } from "react-router-dom";
+import { FaAirbnb } from "react-icons/fa";
 
 export default function Root() {
   return (
-    <h1>
-      Root
+    <Box>
+      <HStack
+        justifyContent={"space-between"}
+        px={10}
+        py={5}
+        borderBottomWidth={1}
+      >
+        <Box color={"red.500"}>
+          <Link to={"/"}>
+            <FaAirbnb size={48} />
+          </Link>
+        </Box>
+        <HStack spacing={2}>
+          <Button colorScheme={"red"}>Sign Up</Button>
+          <Button>Sign In</Button>
+        </HStack>
+      </HStack>
       <Outlet />
-    </h1>
+    </Box>
   );
 }
