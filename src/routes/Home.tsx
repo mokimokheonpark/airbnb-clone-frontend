54844d84fbd97d36d07be620777dd45558db69bf
Mokheon Park <mokimokheonpark@gmail.com>
2023-08-18 16:09:04 -0400

    handled the loading state

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@chakra-ui/react";
+import { Box, Grid, Skeleton, SkeletonText } from "@chakra-ui/react";
 import Room from "../components/Room";
 
 export default function Home() {
@@ -19,12 +19,11 @@ export default function Home() {
         "2xl": "repeat(5, 1fr)",
       }}
     >
-      {[
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-        21, 22, 23, 24, 25, 26, 27, 28,
-      ].map((index) => (
-        <Room key={index} />
-      ))}
+      <Box>
+        <Skeleton height={280} mb={7} rounded={"2xl"} />
+        <SkeletonText w={"50%"} noOfLines={3} />
+      </Box>
+      <Room />
     </Grid>
   );
 }
