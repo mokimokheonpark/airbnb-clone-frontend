c8321a3e1928f92ee00e94ba3eac2716e25daed5
Mokheon Park <mokimokheonpark@gmail.com>
2024-02-14 18:15:01 -0500

    applied chakra-ui on UploadPhotos

--- src/routes/UploadPhotos.tsx
+++ src/routes/UploadPhotos.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
+import { Box, Button, Container, Heading } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import IsHostPage from "../components/IsHostPage";
+import IsLoggedInPage from "../components/IsLoggedInPage";
 import { uploadPhoto } from "../api";
 
 export default function UploadPhotos() {
@@ -35,14 +38,36 @@ export default function UploadPhotos() {
   };
 
   return (
-    <div>
-      <h1>Upload Photo Test</h1>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload} disabled={uploading}>
-        {uploading ? "Uploading..." : "Upload Button"}
-      </button>
-      {error && <div>{error}</div>}
-      {success && <div>Photo uploaded successfully!</div>}
-    </div>
+    <IsLoggedInPage>
+      <IsHostPage>
+        <Box
+          mt={10}
+          pb={40}
+          px={{
+            base: 10,
+            lg: 40,
+          }}
+        >
+          <Container>
+            <Heading textAlign={"center"} mb={50}>
+              Upload Photos
+            </Heading>
+            <input type="file" onChange={handleFileChange} />
+            <Button
+              type="submit"
+              w="full"
+              colorScheme={"red"}
+              mt={50}
+              onClick={handleUpload}
+              disabled={uploading}
+            >
+              {uploading ? "Uploading..." : "Upload the Photo"}
+            </Button>
+            {error && <div>{error}</div>}
+            {success && <div>Photo uploaded successfully!</div>}
+          </Container>
+        </Box>
+      </IsHostPage>
+    </IsLoggedInPage>
   );
 }
